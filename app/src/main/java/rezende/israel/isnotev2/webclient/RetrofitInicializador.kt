a25b0a958e3628cc6e06412025bc824e6f0a11d3
israel <israelrezendeluiz2903@gmail.com>
2022-10-13 17:39:30 -0300

    Incluido Logging Interceptor para monitorar e apresentar informações mais detalhadas das requisições e respostas HTTP.

--- app/src/main/java/rezende/israel/isnotev2/webclient/RetrofitInicializador.kt
+++ app/src/main/java/rezende/israel/isnotev2/webclient/RetrofitInicializador.kt
@@ -1,13 +1,22 @@
 package rezende.israel.isnotev2.webclient
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import rezende.israel.isnotev2.webclient.NotaService.NotaService
 
 class RetrofitInicializador {
 
+    private val client by lazy {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        OkHttpClient.Builder().addInterceptor(logging).build()
+    }
+
+
     private val retrofit = Retrofit.Builder().baseUrl("http://172.19.52.49:8080/")
-        .addConverterFactory(MoshiConverterFactory.create()).build()
+        .addConverterFactory(MoshiConverterFactory.create()).client(client).build()
 
     val notaService = retrofit.create(NotaService::class.java)
 
